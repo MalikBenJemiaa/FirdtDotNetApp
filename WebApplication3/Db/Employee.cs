31f997c49ef0624d10111673836d8e80e2670089
MalikBenJemiaa <melekbenjemiaa05@gmail.com>
2023-12-08 16:18:29 +0100

    first commit

---
+++ WebApplication3/Db/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication3.Db
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ WebApplication3/Migrations/20231208122953_init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebApplication3.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
